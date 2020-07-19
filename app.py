e043d2adba85b67ad98067c2960da140d49b78fd
juanengml <juanengml@gmail.com>
2020-07-19 15:16:42 -0300

    up

---
+++ app.py
@@ -0,0 +1,35 @@
+import cv2
+from bounding_box import bounding_box as bb
+from datetime import datetime as dt
+from farmertech.research.AI import CNN_Model
+import numpy as np
+
+cap = cv2.VideoCapture("../onepig1.mp4")
+
+beberoudo = (554, 101, 166, 367)
+cocho = (0, 0, 355, 212)
+
+np.set_printoptions(suppress=True)
+
+model = CNN_Model(keras_model='keras_model.h5',roi=cocho)
+
+def main():
+   while(True):
+        ret, frame = cap.read()
+        result = model.result(frame)
+        data_value = result[0]
+        image = cv2.resize(frame,(int(720),int(480)))
+        bb.add(image, beberoudo[0], beberoudo[1], beberoudo[0]+beberoudo[2], beberoudo[1]+beberoudo[3], "bebedouro", "aqua")
+        bb.add(image, cocho[0], cocho[1], cocho[0]+ cocho[2], cocho[1]+cocho[3], "COCHO STATUS: " + data_value, "orange")
+        img = image
+        roi_cocho = result[1]
+        cv2.imshow("roi sensor",roi_cocho)
+        cv2.imshow('frame',img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+   cap.release()
+   cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ farmertech/research/AI.py
@@ -0,0 +1,28 @@
+
+import cv2
+import numpy as np
+from datetime import datetime as dt
+import tensorflow.keras
+from PIL import Image, ImageOps
+
+
+
+class CNN_Model(object):
+    def __init__(self,keras_model,roi):
+        self.keras_model = keras_model # keras_model.h5
+        self.model = tensorflow.keras.models.load_model(self.keras_model)
+        self.roi = roi
+
+    def result(self,frame):
+        image = cv2.resize(frame,(int(720),int(480)))
+        cocho = self.roi
+        imCrop = image[cocho[1]:cocho[1]+cocho[3], cocho[0]:cocho[0]+cocho[2]]
+        img = cv2.resize(imCrop,(int(224),int(224)))
+        normalized_image_array = (img.astype(np.float32) / 127.0) - 1
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        data[0] = normalized_image_array
+        prediction = self.model.predict(data)
+        result = prediction.tolist()[0]
+        pesos = [round(result[0],2) ,round(result[1],2)]
+        data_value = "[ porco " + str(pesos[0]) + "] - [ fundo: "+ str(pesos[1])+"]"
+        return [data_value,imCrop]
